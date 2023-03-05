ce1abf06349a49a2764d956bed07aeca5673f199
infrazone <126993062+infrazone@users.noreply.github.com>
2023-03-05 12:49:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group
 
-resource "azurerm_resource_group" "adl_rg" {
+resource "azurerm_resource_group" "iz_rg" {
   name     = "rg-${var.basename}"
   location = var.location
   tags     = var.tags
